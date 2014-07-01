6b0808fd5e07aa5d50c997ecdcba46444eef443b
fecloud <oyf_feng@163.com>
2014-07-01 23:03:57 +0800

    add delete method

--- http/httpd.js
+++ http/httpd.js
@@ -5,50 +5,101 @@ var fs = require("fs");
 var url = require('url');
 var webroot = process.argv[2];
 console.log("webroot:" + webroot);
-function list_dir(path){
-    var files = fs.readdirSync(path);
-    var file_array=[];
-    files.forEach(function(name){
-        //console.log(name);
-        // 查看文件状态
-        var st  = fs.statSync( path + name);
-        if(st.isFile())
-        {
-           var f = new SFile();
-            f.name = name;
-            f.size = st.size;
-            file_array.push(f);
-        }
 
-    });
-    return file_array;
+var SFile= function (){
+    this.name;
+    this.size;
+    this.mtime;
+}
+
+var Result = function(){
+    this.action;
+    this.error = '';
+    this.data;
+}
+
+/**
+ * 返回当前目录的子目录以及文件
+ */ 
+function list_dir(params){
+    var result = new Result();
+    result.action = "list";
+    if(params && params.value){
+        var dir = webroot + "/" + params.value;
+        var files = fs.readdirSync(dir);
+        var file_array=[];
+        files.forEach(function(name){
+            // 查看文件状态
+            var st  = fs.statSync(dir + "/" + name);
+            if(st.isFile()) {
+                var f = new SFile();
+                f.name = name;
+                f.size = st.size;
+                f.mtime = st.mtime.toLocaleString();
+                file_array.push(f);
+            }
+
+        });
+        result.data = file_array;
+    }else {
+        result.error = 'path not found!';
+    }
+    return result;
+}
+
+/**
+ * 删除文件
+ */
+function delete_file(params){
+    var result = new Result();
+    result.action = "delete";
+
+    var path = webroot + "/" + params.value;
+    //&& fs.existsSync(params.value)
+    if(params && params.value && fs.existsSync(path)){
+        console.log('delete file ' + path);
+        result.data = fs.unlinkSync(path);
+    }else {
+        result.error = "check path!";
+    }
+    return result;
 }
 /**
  * 对象封装
  * @constructor
  */
-var SFile= function (){
-    this.name;
-    this.size;
+
+
+function dispath(params){
+    if(params && params.action){
+        if(params.action == 'list'){
+            return list_dir(params);
+        }else if(params.action == 'delete'){
+            return delete_file(params);
+        }else {
+            var result = new result;
+            result.error = 'action not found!';
+            console.error("action not found!");
+            return result;
+        }
+
+    }else {
+        var result = new Result;
+        result.error = 'action require!';
+        console.error("action require!");
+        return result;
+    }
 }
 
+/**
+ * 创建服务
+ */
 http.createServer(function (req, res) {
     console.info('url:' + req.url);
     var params = url.parse(req.url, true).query;
     console.info(params);
-
-    if(params){
-        if(params.action && params.value){
-            //console.log(params.action);
-            var files = list_dir( webroot + params.value);
-            var str_files = JSON.stringify(files);
-            console.info(str_files);
-            res.setHeader('Content-Type','application/json;charset=UTF-8');
-            res.write(str_files);
-        }else {
-            console.error("action not found!");
-        }
-    }
+    res.setHeader('Content-Type','application/json;charset=UTF-8');
+    res.write(JSON.stringify(dispath(params)));
     res.end();
 
 }).listen(3000, '127.0.0.1');
