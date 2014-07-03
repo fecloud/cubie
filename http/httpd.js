3ce5089ff379432a742e0d2f2d5e23e939d65315
oyf_feng <oyf_feng@163.com>
2014-07-03 18:34:05 +0800

    12

--- http/httpd.js
+++ http/httpd.js
@@ -3,10 +3,22 @@
 var http = require('http');
 var fs = require("fs");
 var url = require('url');
+var formidable = require("formidable");
 var webroot = process.argv[2];
-console.log("webroot:" + webroot);
 
-var SFile= function (){
+if (webroot == undefined) {
+    console.error("not setting webroot ,please set !");
+    process.exit(1);
+} else {
+    console.log("webroot:" + webroot);
+}
+
+
+/**
+ * 返回数据
+ * @constructor
+ */
+var SFile = function () {
     this.isFile = false;
     this.isDir = false
     this.name;
@@ -15,7 +27,11 @@ var SFile= function (){
     this.path = "";
 }
 
-var Result = function(){
+/**
+ * JSON 返回数据
+ * @constructor
+ */
+var Result = function () {
     this.action;
     this.error = '';
     this.data;
@@ -23,49 +39,51 @@ var Result = function(){
 
 /**
  * 返回当前目录的子目录以及文件
- */ 
-function list_dir(params){
+ */
+function list_dir(params) {
     var result = new Result();
     result.action = "list";
-    if(params && params.value){
-        var dir = webroot + "/" + params.value;
+    if ( params.value) {
+        //如果客户端传来的数据没有加/,自动加上
+        if (params.value.substring(params.value.length - 1) != '/') {
+            params.value = params.value + '/';
+        }
+        var dir = webroot + params.value;
         var files = fs.readdirSync(dir);
-        var file_array=[];
+        var file_array = [];
 
-        files.forEach(function(name){
+        files.forEach(function (name) {
             // 查看文件状态
-            var st  = fs.statSync(dir + "/" + name);
+            var st = fs.statSync(dir + name);
             var f = new SFile();
-             if(st.isDirectory()){
+            if (st.isDirectory()) {
                 f.isDir = true;
                 f.name = name;
                 f.size = st.size;
                 f.mtime = st.mtime.toLocaleString();
-                f.path = params.value + "/" + name;
+                f.path = params.value + name;
                 file_array.push(f);
             }
 
-           
+
         });
 
-        files.forEach(function(name){
-        // 查看文件状态
-        var st  = fs.statSync(dir + "/" + name);
-        var f = new SFile();
-        if(st.isFile()) {
-            f.isFile = true;
-            f.name = name;
-            f.size = st.size;
-            f.mtime = st.mtime.toLocaleString();
-            f.path = params.value + "/" + name;
-            file_array.push(f);
-        }
+        files.forEach(function (name) {
+            // 查看文件状态
+            var st = fs.statSync(dir + "/" + name);
+            var f = new SFile();
+            if (st.isFile()) {
+                f.isFile = true;
+                f.name = name;
+                f.size = st.size;
+                f.mtime = st.mtime.getTime();
+                f.path = params.value + name;
+                file_array.push(f);
+            }
 
-    });
+        });
 
         result.data = file_array;
-    }else {
-        result.error = 'path not found!';
     }
     return result;
 }
@@ -73,16 +91,16 @@ function list_dir(params){
 /**
  * 删除文件
  */
-function delete_file(params){
+function delete_file(params) {
     var result = new Result();
     result.action = "delete";
 
-    var path = webroot + "/" + params.value;
+    var path = webroot + params.value;
     //&& fs.existsSync(params.value)
-    if(params && params.value && fs.existsSync(path)){
+    if (params && params.value && fs.existsSync(path)) {
         console.log('delete file ' + path);
         result.data = fs.unlinkSync(path);
-    }else {
+    } else {
         result.error = "check path!";
     }
     return result;
@@ -93,37 +111,91 @@ function delete_file(params){
  */
 
 
-function dispath(params){
-    if(params && params.action){
-        if(params.action == 'list'){
-            return list_dir(params);
-        }else if(params.action == 'delete'){
-            return delete_file(params);
-        }else {
-            var result = new result;
+function dispath(req, res, params) {
+    if (params.action) {
+        if (params.action == 'list') {
+
+            resultClient(req,res,list_dir(params));
+
+        } else if (params.action == 'delete') {
+
+            resultClient(req,res,delete_file(params));
+
+        } else if (params.action == 'upload') {
+
+            save_file(req, res);
+
+        } else {
+
+            var result = new Result;
             result.error = 'action not found!';
             console.error("action not found!");
-            return result;
+            resultClient(req, res, result);
+
         }
 
-    }else {
-        var result = new Result;
-        result.error = 'action require!';
-        console.error("action require!");
-        return result;
     }
+}
+
+/**
+ * 保存文件
+ * @param req
+ * @param res
+ * @returns {Result}
+ */
+function save_file(req, res) {
+    var result = new Result();
+    result.action = "upload";
+    if (params.value) {
+        var save_dir = params.value;
+        if (save_dir.substring(save_dir.length - 1) != '/') {
+            save_dir.value = save_dir + '/';
+        }
+        // parse a file upload
+        var form = new formidable.IncomingForm();
+        form.encoding = 'utf-8';
+        form.uploadDir = webroot + save_dir;
+
+        form.parse(req, function (err, fields, files) {
+            console.log(fields);
+            console.log(files);
+            // res.writeHead(200, {'content-type': 'text/plain'});
+            // res.write('received upload:\n\n');
+            //  res.write(util.inspect({fields: fields, files: files}));
+        });
+    } else {
+        result.error = 'require save path!';
+    }
+
+    return result;
+}
+
+/**
+ * 向客户端回复json
+ * @param req
+ * @param res
+ * @param result
+ */
+function resultClient(req, res, result) {
+    res.setHeader('Content-Type', 'application/json;charset=UTF-8');
+    res.write(JSON.stringify(result));
+    res.end();
 }
 
 /**
  * 创建服务
  */
 http.createServer(function (req, res) {
+
     console.info('url:' + req.url);
     var params = url.parse(req.url, true).query;
-    console.info(params);
-    res.setHeader('Content-Type','application/json;charset=UTF-8');
-    res.write(JSON.stringify(dispath(params)));
-    res.end();
+    if (params != undefined) {
+        dispath(req, res, params);
+    } else {
+        params = new Result();
+        params.error = "check request params!";
+        resultClient(req, res, params);
+    }
 
 }).listen(3000, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:3000/');
--- http/static/app.js
+++ http/static/app.js
@@ -1,65 +1,122 @@
-$(document).ready(function(){
-	
-		var path = getArgs(path);
-		if(path == '')
-			path = "/";
-		$.ajax({url:'/fafa.php?action=list&value=' + path,
-				success:function(data){
-						load_list(data);
-					}
-		});
-	
+$(document).ready(function () {
+
+    var path = getArgs(path);
+    if (path == '')
+        path = "/";
+    $.ajax({url: '/' + parseInt(Math.random() * 10000000) + '.php?action=list&value=' + path,
+        success: function (data) {
+            load_list(data);
+        }
+    });
+
+    $('.back_btn').bind('click',function (e){
+        history.go(-1);
+        e.stopPropagation();
+    });
+
 });
 
 function getArgs(strParame) {
-	var query = location.search.substring(1); // Get query string
-	var pairs = query.split("&"); // Break at ampersand
-	for(var i = 0; i < pairs.length; i++) {
-		var pos = pairs[i].indexOf('='); // Look for "name=value"
-		if (pos == -1) 
-			continue; // If not found, skip
-		
-		var argname = pairs[i].substring(0,pos); // Extract the name
-		var value = pairs[i].substring(pos+1); // Extract the value
-		value = decodeURIComponent(value); // Decode it, if needed
-		return  value; // Store as a property
-	}
-	
-	return ""; // Return the object
+    var query = location.search.substring(1); // Get query string
+    var pairs = query.split("&"); // Break at ampersand
+    for (var i = 0; i < pairs.length; i++) {
+        var pos = pairs[i].indexOf('='); // Look for "name=value"
+        if (pos == -1)
+            continue; // If not found, skip
+
+        var argname = pairs[i].substring(0, pos); // Extract the name
+        var value = pairs[i].substring(pos + 1); // Extract the value
+        value = decodeURIComponent(value); // Decode it, if needed
+        return  value; // Store as a property
+    }
+
+    return ""; // Return the object
 }
 
-function load_list(data){
-	if(data && data.error == '' && data.data){
-		var arr = data.data;
-		for(var i = 0, len = arr.length; i < len; i++){  
-			var file = arr[i];
-       		console.log(arr[i]);
-       		var item ;
-       		if(file.isDir){
-       			item = "<li ><a href=\"file.html?path=" + file.path +"\" class=\"list-item\"><i class=\"file-icon folder\"></i><div class=\"content\"><h3> "+ file.name+" </h3><div class=\"list-content\">2013-12-06 19:52:03<span>285.42k</span></div><div class=\"file-operate\"><div class=\"file-rename\" <i class=\"iedit\"></i>重命名</div><div class=\"file-delete\" data-ac=\"active\"><i class=\"iremove\"></i>删除</div></div></div><div class=\"show-operate\"><i class=\"idown\"></i></div></a></li>";
-      		}else {
-      			item = "<li ><a href=\"src" + file.path +"\" class=\"list-item\"><i class=\"file-icon file\"></i><div class=\"content\"><h3> "+ file.name+" </h3><div class=\"list-content\">2013-12-06 19:52:03<span>285.42k</span></div><div class=\"file-operate\"><div class=\"file-rename\" <i class=\"iedit\"></i>重命名</div><div class=\"file-delete\" data-ac=\"active\"><i class=\"iremove\"></i>删除</div></div></div><div class=\"show-operate\"><i class=\"idown\"></i></div></a></li>";
-       		}
-       		$('#content').append(item);
-		}
-	}
-}
-Date.prototype.format =function(format)
-    {
-        var o = {
-        "M+" : this.getMonth()+1, //month
-		"d+" : this.getDate(),    //day
-		"h+" : this.getHours(),   //hour
-		"m+" : this.getMinutes(), //minute
-		"s+" : this.getSeconds(), //second
-		"q+" : Math.floor((this.getMonth()+3)/3),  //quarter
-		"S" : this.getMilliseconds() //millisecond
+function load_list(data) {
+    if (data && data.error == '' && data.data) {
+        var arr = data.data;
+        for (var i = 0, len = arr.length; i < len; i++) {
+            var file = arr[i];
+//            console.log(arr[i]);
+            var item;
+            if (file.isDir) {
+                item = "<li ><a href=\"file.html?path=" + file.path
+                    + "/\" class=\"list-item\"><i class=\"file-icon folder\"></i><div class=\"content\"><h3> " + file.name
+                    + "</h3><div class=\"list-content\">" + new Date(file.mtime).format("yyyy-MM-dd hh:mm:ss")
+                    + "</div><div class=\"file-operate\"><div class=\"file-rename\" > <i class=\"iedit\"></i>重命名</div><div class=\"file-delete\" data-ac=\"active\"><i class=\"iremove\"></i>删除</div></div></div><div class=\"show-operate\"><i class=\"idown\"></i></div></a></li>";
+            } else {
+                item = "<li ><a href=\"src" + file.path
+                    + "\" class=\"list-item\"><i class=\"file-icon file\"></i><div class=\"content\"><h3> " + file.name
+                    + "</h3><div class=\"list-content\">" + new Date(file.mtime).format("yyyy-MM-dd hh:mm:ss")
+                    + "<span>" + renderSize(file.size)
+                    + "</span></div><div class=\"file-operate\"><div class=\"file-rename\" ><i class=\"iedit\"></i>重命名</div><div class=\"file-delete\" data-ac=\"active\"><i class=\"iremove\"></i>删除</div></div></div><div class=\"show-operate\"><i class=\"idown\"></i></div></a></li>";
+            }
+            $('#content').append(item);
+            $(".list-item").bind('click',idown);
         }
-        if(/(y+)/.test(format)) format=format.replace(RegExp.$1,
-        (this.getFullYear()+"").substr(4- RegExp.$1.length));
-        for(var k in o)if(new RegExp("("+ k +")").test(format))
-        format = format.replace(RegExp.$1,
-        RegExp.$1.length==1? o[k] :
-        ("00"+ o[k]).substr((""+ o[k]).length));
-        return format;
     }
+}
+Date.prototype.format = function (format) {
+    var o = {
+        "M+": this.getMonth() + 1, //month
+        "d+": this.getDate(),    //day
+        "h+": this.getHours(),   //hour
+        "m+": this.getMinutes(), //minute
+        "s+": this.getSeconds(), //second
+        "q+": Math.floor((this.getMonth() + 3) / 3),  //quarter
+        "S": this.getMilliseconds() //millisecond
+    }
+    if (/(y+)/.test(format)) format = format.replace(RegExp.$1,
+        (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o)if (new RegExp("(" + k + ")").test(format))
+        format = format.replace(RegExp.$1,
+                RegExp.$1.length == 1 ? o[k] :
+                ("00" + o[k]).substr(("" + o[k]).length));
+    return format;
+}
+
+
+function idown(b){
+    console.log(b.target).closest(".show-operate");
+    b.preventDefault();
+    b.stopPropagation();
+    console.log("idown");
+}
+
+/**
+ * 文件大小格式
+ * @param value
+ * @param p
+ * @param record
+ * @returns {*}
+ */
+function renderSize(value) {
+    if (null == value || value == '') {
+        return "0b";
+    }
+    var unitArr = new Array("B", "K", "M", "G", "T", "P", "E", "Z", "Y");
+    var index = 0;
+
+
+    var srcsize = parseFloat(value);
+    var size = roundFun(srcsize / Math.pow(1024, (index = Math.floor(Math.log(srcsize) / Math.log(1024)))), 2);
+    return size + unitArr[index];
+}
+
+/*
+ 四舍五入保留小数位数
+ numberRound 被处理的数
+ roundDigit  保留几位小数位
+ */
+function roundFun(numberRound, roundDigit)
+{
+    if (numberRound >= 0) {
+        var tempNumber = parseInt((numberRound * Math.pow(10, roundDigit) + 0.5)) / Math.pow(10, roundDigit);
+        return   tempNumber;
+    } else {
+        numberRound1 = -numberRound
+        var tempNumber = parseInt((numberRound1 * Math.pow(10, roundDigit) + 0.5)) / Math.pow(10, roundDigit);
+        return   -tempNumber;
+    }
+}
