5a678ae144231abc80da29d9334062b91eee3062
fecloud <oyf_feng@163.com>
2014-07-15 23:22:49 +0800

    add baidusync

---
+++ node/baiduyunsync/sync.js
@@ -0,0 +1,16 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: ouyangfeng
+ * Date: 7/15/14
+ * Time: 23:19
+ * To change this template use File | Settings | File Templates.
+ */
+
+var util  = require('util');
+
+var APP_ID = 3125236;
+
+var APP_KEY = 'riDL6RfoGHR7j1SrHtZmEqsj';
+
+var SECRET_KEY = 'WC0h9QvmyoyZyKSw2DDkDLEKeoca8Z43';
+
