c88fda5b9cc24ef071b9501727f1dca1db833d78
oyf_feng <oyf_feng@163.com>
2014-07-31 09:44:35 +0800

    add

--- node/photos/pic_resize.js
+++ node/photos/pic_resize.js
@@ -15,32 +15,56 @@ var fs = require('fs')
 var util = require('../util.js');
 
 
-emitter.on("req", function (file, tofile, w, h, func) {
+var resize_queue = [];
+/**
+ * 缩略图
+ * @constructor
+ */
+var PicResize = function () {
+
+    this.file;
+    this.tofile;
+    this.w;
+    this.h;
+    this.callbak;
+
+}
+
+
+emitter.on("req", function (pic_resize) {
+
 
     try {
-        if (!fs.existsSync(tofile)) {
-            util.debug("gm rezie " + file + " tofile" + tofile);
-            gm(file)
-                .resize(w, h)
-                .write(tofile, function (err) {
+        if (!fs.existsSync(pic_resize.tofile)) {
+            //需要resize
+            util.debug("gm rezie " + pic_resize.file + " tofile" + pic_resize.tofile);
+            gm(pic_resize.file)
+                .resize(pic_resize.w, pic_resize.h)
+                .write(pic_resize.tofile, function (err) {
                     if (err) {
-                        if (func)
-                            func.call(func, false);
+                        if (pic_resize.callbak)
+                            pic_resize.callbak.call(pic_resize.callbak, false);
 
-                        util.debug("gm rezie " + file + " tofile" + tofile + " error " + err.toString());
+                        util.debug("gm rezie " + pic_resize.file + " tofile" + pic_resize.tofile + " error " + err.toString());
                     } else {
-                        if (func)
-                            func.call(func, true);
+                        if (pic_resize.callbak)
+                            pic_resize.callbak.call(pic_resize.callbak, true);
 
-                        util.debug("gm rezie " + file + " tofile" + tofile + " success");
+                        util.debug("gm rezie " + pic_resize.file + " tofile" + pic_resize.tofile + " success");
                     }
                 });
+        } else {
+            //已经在在
+            if (pic_resize.callbak)
+                pic_resize.callbak.call(pic_resize.callbak, true);
+
+            util.debug("gm rezie " + pic_resize.file + " tofile" + pic_resize.tofile + " not need resize");
         }
     } catch (error) {
-        if (func)
-            func.call(func, false);
+        if (pic_resize.callbak)
+            pic_resize.callbak.call(pic_resize.callbak, false);
 
-        util.debug("gm rezie " + file + " tofile" + tofile + " catch error");
+        util.debug("gm rezie " + pic_resize.file + " tofile" + pic_resize.tofile + " catch error");
     }
 
 
@@ -53,7 +77,33 @@ emitter.on("req", function (file, tofile, w, h, func) {
 function req_rezie(file, tofile, w, h, func) {
 
     util.debug("gm rezie " + file + " tofile" + tofile + " req_rezie");
-    emitter.emit('req', file, tofile, w, h, func);
+    var need_emitter = false;
+    if (resize_queue.length == 0) {
+        need_emitter = true;
+    }
+
+    var req_resize = new PicResize();
+    req_resize.file = file;
+    req_resize.tofile = tofile;
+    req_resize.w = w;
+    req_resize.h = h;
+    req_resize.callbak = function () {
+
+        util.debug("emitter work one req next");
+        var re = resize_queue.shift();
+        util.debug("get next re" + re);
+        if (re) {
+            emitter.emit('req', re);
+        }
+
+    };
+    resize_queue.push(req_resize);
+
+    if (need_emitter) {
+        util.debug("emitter not work emit");
+        emitter.emit('req', resize_queue.shift());
+
+    }
 
 }
 
