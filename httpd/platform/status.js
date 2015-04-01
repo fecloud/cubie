1846c19a736b4ea865e50261562382a8a6808425
fecloud <oyf_feng@163.com>
2015-04-01 21:04:02 +0800

    modfiy

--- httpd/platform/status.js
+++ httpd/platform/status.js
@@ -159,7 +159,7 @@ function start_service(req, res, params) {
 
     var result = new com.web_result();
 
-    var cmd = "service " + params.value + " start";
+    var cmd = "service " + params.values + " start";
     if (params.sub) {
         cmd = cmd + " " +  params.sub;
     }
@@ -193,7 +193,7 @@ function stop_service(req, res, params) {
 
     var result = new com.web_result();
 
-    var cmd = "service " + params.value + " stop";
+    var cmd = "service " + params.values + " stop";
     if (params.sub) {
         cmd = cmd + " " +  params.sub;
     }
